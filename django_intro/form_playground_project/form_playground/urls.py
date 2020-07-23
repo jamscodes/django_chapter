a11d72a7e19825bfc0dcb14acb36fc5b931bd6e0
JMoore <jmoore.codes@gmail.com>
2020-07-23 10:23:25 -0500

    Add redirect to form_handler()

--- django_intro/form_playground_project/form_playground/urls.py
+++ django_intro/form_playground_project/form_playground/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('result/', views.form_handler)
+    path('result/', views.form_handler),
+    path('form_submitted/', views.form_submission)
 ]
--- django_intro/form_playground_project/form_playground/views.py
+++ django_intro/form_playground_project/form_playground/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 
 # Create your views here.
 
@@ -6,12 +6,9 @@ def index(request):
     return render(request, 'index.html')
 
 def form_handler(request):
-    context = {
-        'name': request.POST['name'],
-        'fav_rock': request.POST['fav_rock'],
-        'mult_fav_rock': request.POST.get('mult_fav_rock', False),
-        'past_pet_rock': request.POST.get('past_pet_rock', False),
-        'cur_pet_rock': request.POST.get('cur_pet_rock', False),
-        'message': request.POST['message']
-    }
-    return render(request, 'form_submitted.html', context)
+    request.session['name'], request.session['fav_rock'], request.session['mult_fav_rock'], request.session['past_pet_rock'], request.session['cur_pet_rock'], request.session['message'] = request.POST['name'], request.POST['fav_rock'], request.POST.get('mult_fav_rock', False), request.POST.get('past_pet_rock', False), request.POST.get('cur_pet_rock', False), request.POST['message']
+
+    return redirect('/form_submitted/')
+
+def form_submission(request):
+    return render(request, 'form_submitted.html')
