c9aaf7ff7d79b894be86f52bd03abb8eeb7575da
JMoore <jmoore.codes@gmail.com>
2020-07-23 11:18:17 -0500

    Create random string generator methods

--- django_intro/form_playground_project/random_word/urls.py
+++ django_intro/form_playground_project/random_word/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('random_word/', views.generate_rand_string)
+    path('random_word/', views.generate_rand_string),
+    path('reset/', views.reset)
 ]
