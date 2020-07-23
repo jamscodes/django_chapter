1b6b4a832fa264190c79b6940803615ed4ba90e5
JMoore <jmoore.codes@gmail.com>
2020-07-23 17:17:26 -0500

    Make ninja gold game logic

--- django_intro/ninja_gold/ninja_gold_game/urls.py
+++ django_intro/ninja_gold/ninja_gold_game/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('process_money/', views.process_money)
+    path('process_money/', views.process_money),
+    path('reset/', views.reset),
+    path('start_game/', views.start_game),
+    path('end_screen/', views.end_screen)
 ]
