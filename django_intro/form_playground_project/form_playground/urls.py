b8ad4a6d7fd6afea9a09669fbf1567ed2c26eaf6
JMoore <jmoore.codes@gmail.com>
2020-07-22 20:19:05 -0500

    Improve aesthetic of form page with HTML and CSS

--- django_intro/form_playground_project/form_playground/urls.py
+++ django_intro/form_playground_project/form_playground/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('result/', views.form_handler)
 ]
--- django_intro/form_playground_project/form_playground/views.py
+++ django_intro/form_playground_project/form_playground/views.py
@@ -3,4 +3,11 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("This is a response!")
+    return render(request, 'index.html')
+
+def form_handler(request):
+    context = {
+        'field_1': request.POST['one'],
+        'field_2': request.POST['two']
+    }
+    return render(request, 'form_submitted.html', context)
