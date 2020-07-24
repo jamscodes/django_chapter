5272a994bd789aa6e4fd8b5882f51bc8d14a625c
JMoore <jmoore.codes@gmail.com>
2020-07-23 23:30:31 -0500

    Add multiple apps to one project and re-route routes

--- django_intro/second_django_project/second_django_project/settings.py
+++ django_intro/second_django_project/second_django_project/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'my_app',
+    'surveys',
+    'users',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- django_intro/second_django_project/second_django_project/urls.py
+++ django_intro/second_django_project/second_django_project/urls.py
@@ -13,8 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path, include
+from django.urls import path, include, re_path
 
 urlpatterns = [
-    path('', include('my_app.urls'))
+    path('blog/', include('my_app.urls')),
+    path('surveys/', include('surveys.urls')),
+    path('', include('users.urls')),
 ]
