2f4fa4816ffd11e71a7be01586aa9a81a430f02b
JMoore <jmoore.codes@gmail.com>
2020-07-23 22:11:37 -0500

    Make unnecessary use of naming routes

--- django_intro/form_playground_project/form_playground/urls.py
+++ django_intro/form_playground_project/form_playground/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('result/', views.form_handler),
+    path('', views.index, name='home'),
+    path('result/', views.form_handler, name='handler'),
     path('form_submitted/', views.form_submission)
 ]
