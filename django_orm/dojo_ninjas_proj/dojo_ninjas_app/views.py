cb879705815215b3153c9a1fb88ec2ad65278170
JMoore <jmoore.codes@gmail.com>
2020-07-29 14:25:33 -0500

    Connect homepage to database

--- django_orm/dojo_ninjas_proj/dojo_ninjas_app/views.py
+++ django_orm/dojo_ninjas_proj/dojo_ninjas_app/views.py
@@ -1,6 +1,27 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
+from .models import Dojo, Ninja
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'home.html')
+    context = {
+        'dojos': Dojo.objects.all(),
+        'ninjas': Ninja.objects.all()
+    }
+    return render(request, 'home.html', context)
+
+def add_dojo(request):
+    Dojo.objects.create(
+        name=request.POST['d_name'],
+        city=request.POST['d_city'],
+        state=request.POST['d_state']
+    )
+    return redirect('/')
+
+def add_ninja(request):
+    Ninja.objects.create(
+        dojo_id=Dojo.objects.get(id=request.POST.get('dojo', False)),
+        first_name=request.POST['n_f_name'],
+        last_name=request.POST['n_l_name']
+    )
+    return redirect('/')
