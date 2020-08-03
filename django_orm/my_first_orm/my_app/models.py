f51ff82e331247fbfd49f394c681ee4711beea18
JMoore <jmoore.codes@gmail.com>
2020-08-03 12:24:57 -0500

    Add Actor model and create m-to-m relationship to Movie model

--- django_orm/my_first_orm/my_app/models.py
+++ django_orm/my_first_orm/my_app/models.py
@@ -12,3 +12,13 @@ class Movie(models.Model):
 
     def __repr__(self):
         return "Title: {}".format(self.title)
+
+class Actor(models.Model):
+    f_name = models.CharField(max_length=45)
+    l_name = models.CharField(max_length=45)
+    movies = models.ManyToManyField(Movie, related_name="actors")
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+
+    def __repr__(self):
+        return f"Name: {self.f_name} {self.l_name}"
