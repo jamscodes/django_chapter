ed8d7a92921862b48d18bd7a7a841d2f4d815256
JMoore <jmoore.codes@gmail.com>
2020-08-08 22:09:10 -0500

    Add validation to 'add_show' and 'edit_show'

--- django_orm/tv_shows_proj/tv_shows_app/models.py
+++ django_orm/tv_shows_proj/tv_shows_app/models.py
@@ -1,18 +1,25 @@
 from django.db import models
+import datetime
 
 # Create your models here.
 
 class ShowManager(models.Manager):
     def validator(self, postData):
         errors = {}
+        cur_date = datetime.datetime.today()
+        post_date = postData.get('rel_date')
+        if len(Movie.objects.filter(title=postData["title"])) > 0:
+            errors['unique'] = 'This show already exists' # this is put in for a bonus goal and it's dumb because it doesn't allow you to edit a show without changing the title
         if len(postData['title']) < 2:
             errors['title'] = 'Title should be at least 2 characters'
-        if len(postData['network']) < 2:
-            errors['network'] = 'Network should be at least 2 characters'
-        if len(postData.get('rel_date')) < 2:
-            errors['rel_date'] = 'Please input full release date'
-        if len(postData['desc']) < 2:
-            errors['desc'] = 'Description should be at least 2 characters'
+        if len(postData['network']) < 3:
+            errors['network'] = 'Network should be at least 3 characters'
+        if len(post_date) < 1:
+            errors['rel_date'] = 'Release date should be a full date'
+        if 'rel_date' not in errors and datetime.datetime.strptime(post_date, "%Y-%m-%d") >= cur_date:
+            errors['past_date'] = 'Release date should be in the past'
+        if len(postData['desc']) > 0 and len(postData['desc']) <= 10:
+            errors['desc'] = 'Description should be at least 10 characters.'
         return errors
 
 class Movie(models.Model):
