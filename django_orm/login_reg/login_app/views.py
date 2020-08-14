f53c3edff37402bcc69b82e2065de4f6db5fa6c3
JMoore <jmoore.codes@gmail.com>
2020-08-14 17:01:26 -0500

    Create login_reg templates with styles

--- django_orm/login_reg/login_app/views.py
+++ django_orm/login_reg/login_app/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('You did it!!')
+    return render(request, 'login.html')
+
+def success(request):
+    return render(request, 'welcome.html')
